298d4e008d92fd79be683c7a070faa1f2b89f5d0
Hookseye <rakagoodridge@gmail.com>
2025-09-07 21:30:59 +0700

    init

---
+++ setting.js
@@ -0,0 +1,10 @@
+// made by 2ky(@s2skky)
+
+let hiterror_switch = false;
+let hitcount_switch = true;
+
+let hiterror_horizontal_margin = '300px';
+let hiterror_vertical_margin = '440px';
+
+let hitcount_horizontal_margin = '0px';
+let hitcount_vertical_margin = '200px';
